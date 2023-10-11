0b45af287242021c7312a41cb135a09061163f29
ProjectWJ <90522466+ProjectWJ@users.noreply.github.com>
2023-10-12 00:35:23 +0900

    Update ATM_11399.java

--- src/ATM_11399.java
+++ src/ATM_11399.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class ATM{
+public class ATM_11399{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
