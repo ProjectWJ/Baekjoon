a4341d0d73cbaad3aefa9a1bc07fda649b4c72bb
ProjectWJ <90522466+ProjectWJ@users.noreply.github.com>
2023-10-20 00:07:50 +0900

    Update 막대기_1094.java

--- src/막대기_1094.java
+++ src/막대기_1094.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class Main1094 {
+public class 막대기_1094 {
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
